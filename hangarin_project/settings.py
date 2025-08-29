86b73a8910f9c8780e60f94036703b72a1aa5ff3
Arenio-lenneth <202380350@psu.palawan.edu.ph>
2025-08-29 12:08:01 +0800

    Added PythonAnywhere domain to ALLOWED_HOST

--- hangarin_project/settings.py
+++ hangarin_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wdhk6gwdu_9w-w-^161&py54(v&$v7bgwg03zorzz6xrttc*(y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ArenioLenneth.pythonanywhere.com']
 
 
 # Application definition
