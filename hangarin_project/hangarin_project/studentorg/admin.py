59e26eab75dda3b27b2044d561f8f92caf1da859
Arenio-lenneth <202380350@psu.palawan.edu.ph>
2025-10-13 12:23:09 +0800

    my changes

--- hangarin_project/hangarin_project/studentorg/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Task, Substask, Category, Note, Priority
-
-admin.site.register(Task)
-admin.site.register(Substask)
-admin.site.register(Category)
-admin.site.register(Note)
-admin.site.register(Priority)
